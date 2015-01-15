b5a50f3cf760fd7ed1ba9f62d578cfb8d6ef341e
Brandon <wilkb777@gmail.com>
2015-01-14 19:39:17 -0600

    update for testing settings

--- src/oct/analysis/application/FoveaFindingExp.java
+++ src/oct/analysis/application/FoveaFindingExp.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.ListIterator;
+import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -38,14 +40,14 @@ import org.jfree.data.xy.XYSeriesCollection;
  * @author www.codejava.net
  *
  */
-public class XYLineChartExample extends JFrame {
+public class FoveaFindingExp extends JFrame {
 
     final JPanel chartPanel;
 
-    public XYLineChartExample() throws IOException {
+    public FoveaFindingExp(String title, BufferedImage img) throws IOException {
         super("XY Line Chart Example with JFreechart");
 
-        chartPanel = createChartPanel(false);
+        chartPanel = createChartPanel(title, img);
         add(chartPanel, BorderLayout.CENTER);
 
         setSize(640, 480);
@@ -53,47 +55,42 @@ public class XYLineChartExample extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    public JPanel createChartPanel(boolean addNew) throws IOException {
-        String chartTitle = "Objects Movement Chart";
-        String xAxisLabel = "X";
-        String yAxisLabel = "Y";
+    public final JPanel createChartPanel(String title, BufferedImage img) throws IOException {
+        String xAxisLabel = "Pixel";
+        String yAxisLabel = "2nd Derivative";
 
-        XYDataset dataset = createDataset(addNew);
-
-        JFreeChart chart = ChartFactory.createXYLineChart(chartTitle,
-                xAxisLabel, yAxisLabel, dataset);
-//        XYSplineRenderer renderer = new XYSplineRenderer(10);
-//        renderer.setSeriesLinesVisible(0, true);
-//        renderer.setSeriesShapesVisible(0, false);
-//        renderer.setSeriesLinesVisible(1, false);
-//        renderer.setSeriesShapesVisible(1, true);
-//        chart.getXYPlot().setRenderer(renderer);
-
+        XYDataset dataset = createDataset(img);
+        JFreeChart chart = ChartFactory.createXYLineChart(title, xAxisLabel, yAxisLabel, dataset);
         ChartPanel panel = new ChartPanel(chart);
-        panel.setPreferredSize(new Dimension(600, 600));
+        panel.setPreferredSize(new Dimension(640, 480));
         panel.setFillZoomRectangle(true);
         panel.setMouseWheelEnabled(true);
         return panel;
     }
 
-    public XYDataset createDataset(boolean addNewData) throws IOException {
+    public XYDataset createDataset(BufferedImage img) throws IOException {
         XYSeriesCollection dataset = new XYSeriesCollection();
-        XYSeries series1 = new XYSeries("Segment Diff.");
-
-//        BufferedImage img = TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\DiseasedOCTs\\DH_10160_OD_L_7_0_02_529disp_fr66reg_fr47-83_AL24p11.tif"));
-//        BufferedImage img = TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\Normal\\AVG_RW_10174_OD_L_7_0_02_529disp_25frames.tif"));
-//        BufferedImage img = TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\Normal\\AVG_RW_10174_OD_L_7_90_05_529disp_27frames.tif"));
-        BufferedImage img = TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\Normal\\AVG_RW_10174_OS_Raw_L_90.tif"));
 
         //get the contour of the ILM
         ArrayList<Point> ilmSeg = new ArrayList<>(getSurfaceSegment(img, 1, Segmentation.ILM_SEGMENT));
+        XYSeries ilm = new XYSeries("ILM Segment");
+        ilmSeg.forEach((p) -> {
+            ilm.add(p.getX(), 480 - p.getY());
+        });
+//        dataset.addSeries(ilm);
         //get the contour of the Brooks Membrane
         ArrayList<Point> brmSeg = new ArrayList<>(getSurfaceSegment(img, 1, Segmentation.BrM_SEGMENT));
+        XYSeries brm = new XYSeries("BrM Segment");
+        brmSeg.forEach((p) -> {
+            brm.add(p.getX(), 480 - p.getY());
+        });
+//        dataset.addSeries(brm);
         //calcualte the difference (in the Y value) between at each point along the X axis for the above contours
         ListIterator<Point> ilmIter = ilmSeg.listIterator();
         ListIterator<Point> brmIter = brmSeg.listIterator();
         double[] x = new double[ilmSeg.size()];
         double[] y = new double[ilmSeg.size()];
+        XYSeries series1 = new XYSeries("Segment Diff.");
         for (int i = 0; ilmIter.hasNext(); i++) {
             Point ilmPoint = ilmIter.next();
             Point brmPoint = brmIter.next();
@@ -104,11 +101,11 @@ public class XYLineChartExample extends JFrame {
             x[i] = xPos;
             y[i] = yPos;
         }
-        dataset.addSeries(series1);
+//        dataset.addSeries(series1);
 
         //build spline function to help find derivaties of diff.
-//        UnivariateInterpolator interpolator = new LoessInterpolator(0.1, 0);//good for identifying the true foveal center
-        UnivariateInterpolator interpolator = new LoessInterpolator(0.35,2);//good for removing and identifying relative vicinity of foveal center
+        UnivariateInterpolator interpolator = new LoessInterpolator(0.4, 0);//good for identifying the true foveal center
+//        UnivariateInterpolator interpolator = new LoessInterpolator(0.2, 0);//good for removing and identifying relative vicinity of foveal center
         UnivariateFunction function = interpolator.interpolate(x, y);
 
         //get first and second derivative second derivative of the diff
@@ -132,18 +129,31 @@ public class XYLineChartExample extends JFrame {
             fd.add(xRealValue, yd.getPartialDerivative(1));//first derivative at point
             sd.add(xRealValue, yd.getPartialDerivative(2));//second derivative at point
         }
-        dataset.addSeries(interpolated);
-        dataset.addSeries(fd);
+//        dataset.addSeries(interpolated);
+//        dataset.addSeries(fd);
         dataset.addSeries(sd);
 
         return dataset;
     }
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        XYLineChartExample myFrame = new XYLineChartExample();
-        SwingUtilities.invokeLater(() -> {
-            myFrame.setVisible(true);
-        });
+        //map of input OCTs, some normal some diseased
+        HashMap<String, BufferedImage> octMap = new HashMap<>();
+        octMap.put("RW_10174_OD_L_7_0", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\Normal\\AVG_RW_10174_OD_L_7_0_02_529disp_25frames.tif")));
+        octMap.put("RW_10174_OD_L_7_90", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\Normal\\AVG_RW_10174_OD_L_7_90_05_529disp_27frames.tif")));
+        octMap.put("RW_10174_OS", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\Normal\\AVG_RW_10174_OS_Raw_L_90.tif")));
+        octMap.put("DH_10160_OD", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\DiseasedOCTs\\DH_10160_OD_L_7_0_02_529disp_fr66reg_fr47-83_AL24p11.tif")));
+        octMap.put("JC_10193_OD", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\DiseasedOCTs\\JC_10193_OD_L_7_0_0000002_OCT_600x1000_fr90reg_fr80-100_resized_810x460.tif")));
+        octMap.put("KS_10175_OS", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\DiseasedOCTs\\KS_10175_OS_L_7_0_24_529disp_fr38reg_fr28-70_AL24p2.tif")));
+        octMap.put("KS_10238_OS", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\DiseasedOCTs\\KS_10238_OS_L_7_90_05_529disp_reg_fr1-25_AL21p35.tif")));
+        octMap.put("KS_10243_OD", TiffReader.readTiffImage(new File("D:\\Documents\\IdependentContracting\\Carrol Lab\\LRP Analysis App\\Example Human OCTs\\DiseasedOCTs\\KS_10243_OD_L_7_0_02_529disp_reg_fr1-14_AL22p43.tif")));
+
+        for (Map.Entry<String, BufferedImage> ent : octMap.entrySet()) {
+            FoveaFindingExp myFrame = new FoveaFindingExp(ent.getKey(), ent.getValue());
+            SwingUtilities.invokeLater(() -> {
+                myFrame.setVisible(true);
+            });
+        }
 
 //        System.out.println("Removeing old stuff...");
 //        myFrame.remove(myFrame.chartPanel);
